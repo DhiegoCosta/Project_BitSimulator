c7c2bd979ebb1b2404092f976631e460cbf9cc9d
dhicosta <dhiegofp14@gmail.com>
2020-10-24 14:11:09 -0300

    Testando

---
+++ js/index.js
@@ -0,0 +1,20 @@
+function IniciarPercusoDeInstrucoes() {
+   	intervalosDeChamandaDeFuncao = setInterval(game, VelocidadeDeChamadaDeFuncao);
+}
+function game()
+{
+	ContadorDeEspacoPercorrido += 100;
+	ObjectMov.setAttribute("cx", ContadorDeEspacoPercorrido);
+	if(ContadorDeEspacoPercorrido == 1000) {
+   		clearInterval(intervalosDeChamandaDeFuncao);
+   		console.log("fim de jogo!!");
+   		ObjectMov.setAttribute("cx", 1200);
+   		ContadorDeEspacoPercorrido = 0;
+   	}
+}
+function AdicionarComandoNaMemoria() {
+
+}
+function ExecutarPercursoEmAleatorio() {
+
+}
